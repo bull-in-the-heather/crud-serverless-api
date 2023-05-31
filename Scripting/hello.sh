279ebcddbc619008c7b81195f6616c2c28a3e3f5
nude <akabek0@proton.me>
2023-05-31 06:13:24 -0400

    file cleanup

--- Scripting/hello.sh
+++
@@ -1,9 +0,0 @@
-# /bin/bash
-
-# this is a comment
-name=Neiman 
-echo "Hello World" # this is also a comment
-echo "My name is $name"
-echo "I'm using $BASH $BASH_VERSION to learn shell scripting. It's really fun."
-echo "Our home directory is $HOME."
-echo "Our current working directory is $PWD."
--- Scripting/trick.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-echo "Enter name : "
-read
-echo "name : $REPLY"
